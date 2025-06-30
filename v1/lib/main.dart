c92061ada655b59926419012c58da6f8085308c5
MrBillullo <javier@billullo.com>
2025-06-30 18:55:50 -0400

    MAc Os app

--- v1/lib/main.dart
+++ v1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
+import 'package:go_router/go_router.dart';
 import 'package:v1/pages/agent_page.dart';
 import 'package:v1/pages/network.dart';
 import 'package:v1/themes/theme.dart';
@@ -21,6 +22,42 @@ Future main() async {
   runApp(const MyApp());
 }
 
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomePage();
+      },
+    ),
+    GoRoute(
+      path: '/chat',
+      builder: (BuildContext context, GoRouterState state) => LunaChatPage(),
+    ),
+    GoRoute(
+      path: '/voice',
+      builder: (BuildContext context, GoRouterState state) => const VoicePage(),
+    ),
+    GoRoute(
+      path: '/vision',
+      builder: (BuildContext context, GoRouterState state) => const VisionPage(),
+    ),
+    GoRoute(
+      path: '/dashboard',
+      builder: (BuildContext context, GoRouterState state) => const Dashboard(),
+    ),
+    GoRoute(
+      path: '/agent',
+      builder: (BuildContext context, GoRouterState state) => AgentPage(),
+    ),
+    GoRoute(
+      path: '/network',
+      builder: (BuildContext context, GoRouterState state) => NetworkPage(),
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -30,20 +67,11 @@ class MyApp extends StatelessWidget {
     TextTheme textTheme = createTextTheme(context, "Inter", "Inter");
     MaterialTheme theme = MaterialTheme(textTheme);
 
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       debugShowCheckedModeBanner: false,
       title: 'Luna AI Suite',
       theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-        '/chat': (context) => LunaChatPage(),
-        '/voice': (context) => const VoicePage(),
-        '/vision': (context) => const VisionPage(),
-        '/dashboard': (context) => const Dashboard(),
-        '/agent': (context) => AgentPage(),
-        '/network': (context) => NetworkPage(),
-      },
     );
   }
 }
